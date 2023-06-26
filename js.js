2f7b863055171d0ee331bf7df39eb81c9ff6b653
TheWhiteHackers <nurefsanbera@gmail.com>
2023-06-26 15:20:03 -0500

    the wheel

--- js.js
+++ js.js
@@ -34,3 +34,88 @@ function createabc(){
     }
     document.getElementById("letters").innerHTML=str+"</center>";;
 }
+
+
+var wordlist = [
+    '$950',
+    'ONE MILLION $',
+    'LOSE A TURN',
+    '$800',
+    '$500',
+    '$650',
+    'ONE MILLION $',
+    '$900',
+    'BANKRUPT',
+    '$2,500',
+    '$500',
+    '$900',
+    '$700',
+    'ONE MILLION $',
+    '$650',
+    '$500',
+    '$700',
+    '$500',
+    '$600',
+    '$550',
+    'ONE MILLION $',
+    '$600',
+    'BANKRUPT',
+    '$650',
+  ]
+  
+  function buildSlotItem (text) {
+      return $('<div>').addClass('slottt-machine-recipe__item')
+                       .text(text)
+  }
+  
+  function buildSlotContents ($container, wordlist) {
+    $items = wordlist.map(buildSlotItem);
+    $container.append($items);
+  }
+  
+  function popPushNItems ($container, n) {
+      $children = $container.find('.slottt-machine-recipe__item');
+      $children.slice(0, n).insertAfter($children.last());
+  
+      if (n === $children.length) {
+        popPushNItems($container, 1);
+      }
+  }
+  
+  // After the slide animation is complete, we want to pop some items off
+  // the front of the container and push them onto the end. This is
+  // so the animation can slide upward infinitely without adding
+  // inifinte div elements inside the container.
+  function rotateContents ($container, n) {
+      setTimeout(function () {
+        popPushNItems($container, n);
+        $container.css({top: 0});
+      }, 300);    
+  }
+  
+  function randomSlotttIndex(max) {
+    var randIndex = (Math.random() * max | 0);
+    return (randIndex > 10) ? randIndex : randomSlotttIndex(max);
+  }
+  
+    
+    
+  function animate() {
+    var wordIndex = randomSlotttIndex(wordlist.length);
+    $wordbox.animate({top: -wordIndex*150}, 500, 'swing', function () {
+      rotateContents($wordbox, wordIndex);
+    });
+  }
+  
+
+ function wheel(){
+    console.log("hi!");
+    $wordbox = $('#wordbox .slottt-machine-recipe__items_container');
+    buildSlotContents($wordbox, wordlist);  
+    buildSlotContents($wordbox, wordlist);  
+    buildSlotContents($wordbox, wordlist);  
+    buildSlotContents($wordbox, wordlist);  
+    
+    setInterval(animate, 2000);
+};
+
