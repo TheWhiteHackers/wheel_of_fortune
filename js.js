ce2a716ae706338fe749bb2ce566eac3253a75cb
TheWhiteHackers <nurefsanbera@gmail.com>
2023-08-01 09:30:11 -0500

    small changes

--- js.js
+++ js.js
@@ -72,49 +72,49 @@ var wordlist = [
     '$650',
   ]
   
-  function buildSlotItem (text) {
+function buildSlotItem (text) {
       return $('<div>').addClass('slottt-machine-recipe__item')
                        .text(text)
-  }
+}
   
-  function buildSlotContents ($container, wordlist) {
+function buildSlotContents ($container, wordlist) {
     $items = wordlist.map(buildSlotItem);
     $container.append($items);
-  }
+}
   
-  function popPushNItems ($container, n) {
+function popPushNItems ($container, n) {
       $children = $container.find('.slottt-machine-recipe__item');
       $children.slice(0, n).insertAfter($children.last());
   
       if (n === $children.length) {
         popPushNItems($container, 1);
       }
-  }
+}
   
   // After the slide animation is complete, we want to pop some items off
   // the front of the container and push them onto the end. This is
   // so the animation can slide upward infinitely without adding
   // inifinte div elements inside the container.
-  function rotateContents ($container, n) {
+function rotateContents ($container, n) {
       setTimeout(function () {
         popPushNItems($container, n);
         $container.css({top: 0});
       }, 300);    
-  }
+}
   
-  function randomSlotttIndex(max) {
+function randomSlotttIndex(max) {
     var randIndex = (Math.random() * max | 0);
     return (randIndex > 10) ? randIndex : randomSlotttIndex(max);
-  }
+}
   
-  function animate() {
+function animate() {
     var wordIndex = randomSlotttIndex(wordlist.length);
     $wordbox.animate({top: -wordIndex*150}, 500, 'swing', function () {
       rotateContents($wordbox, wordIndex);
     });
-  }
+}
   
- function wheel(){
+function wheel(){
     console.log("hi!");
     $wordbox = $('#wordbox .slottt-machine-recipe__items_container');
     buildSlotContents($wordbox, wordlist);  
@@ -165,7 +165,9 @@ function drawsen(){
     }
   }
 }
-
+function explaincore(){
+  
+}
 
 
 
